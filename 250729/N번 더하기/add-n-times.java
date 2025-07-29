6185f1ba382b6f5f665f50d43b0aff61863788fe
soohoioa <151589776+soohoioa@users.noreply.github.com>
2025-07-29 16:49:19 +0900

    Title: N번 더하기, Time: 134ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250729/N번 더하기/add-n-times.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int N = sc.nextInt();
+    
+
+        for (int i = 1; i <= N; i++) {
+            int sum = A + N * i;
+            System.out.println(sum);
+        }
+    }
+}
