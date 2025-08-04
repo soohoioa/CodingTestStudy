d8b8fb6f8d8bba79426868255b445476fcc73ce6
soohoioa <151589776+soohoioa@users.noreply.github.com>
2025-08-04 13:53:47 +0900

    Title: 정상적인 구구단, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250804/정상적인 구구단/normal-multiple.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                System.out.print(i + " * " + j + " = " + (j * i));
+                if (j != N) {
+                    System.out.print(", ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
