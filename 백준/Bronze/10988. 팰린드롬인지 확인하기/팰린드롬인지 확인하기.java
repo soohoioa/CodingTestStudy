ee5c50c82fff77f7bca8cae4e2548e7335f67b7d
soohoioa <151589776+soohoioa@users.noreply.github.com>
2026-01-05 00:35:00 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 104 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                System.out.println(0);
+                return;
+            }
+            left++;
+            right--;
+        }
+
+        System.out.println(1);
+    }
+}
