83639af3aa45e3e43410c78f35f0f1d673a5c497
soohoioa <151589776+soohoioa@users.noreply.github.com>
2026-01-08 01:36:26 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 100 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        String[] cro = {"dz=", "c=", "c-", "d-", "lj", "nj", "s=", "z="};
+
+        for (String c : cro) {
+            s = s.replace(c, "*"); 
+        }
+
+        System.out.println(s.length());
+    }
+}
