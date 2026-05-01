ca2b0473683808d2455d3478620e675c0849ca71
soohoioa <151589776+soohoioa@users.noreply.github.com>
2026-05-01 15:57:25 +0900

    [Passed] 반복 출력하기 (135ms, 10MB)

---
+++ 260501/반복 출력하기/repeated-output.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void number(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println("12345^&*()_");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        number(num);
+    }
+}
