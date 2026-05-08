5e871e9a3bd14c6e3c77408e82155ef085a8d1af
soohoioa <151589776+soohoioa@users.noreply.github.com>
2026-05-08 14:39:42 +0900

    [level 0] Title: 수 조작하기 2, Time: 9.09 ms, Memory: 85.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(int[] numLog) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i < numLog.length; i++) {
+            int diff = numLog[i] - numLog[i - 1];
+
+            switch (diff) {
+                case 1: sb.append("w"); break;
+                case -1: sb.append("s"); break;
+                case 10: sb.append("d"); break;
+                case -10: sb.append("a"); break;
+            }
+        }
+
+        return sb.toString();
+    }
+}
